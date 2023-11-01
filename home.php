1efac9c3517a5d1920436f0958bf4de47d37084b
Bruno Coelho <eu.brunocoelho94@gmail.com>
2023-11-01 14:49:53 -0300

    updated headers

--- home.php
+++ home.php
@@ -4,7 +4,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: access');
 header('Access-Control-Allow-Methods: GET');
 header('Content-Type: application/json; charset=UTF-8');
-header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Request-With');
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
 
 require_once __DIR__ . '/database.php';
 require_once __DIR__ . '/jwtHandler.php';
